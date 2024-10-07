3b508ad999335b0c5e21ca96d2a8740f034fdd95
Anderson Barrientos <91146611+abarrientosGAP@users.noreply.github.com>
2024-10-06 22:13:27 -0500

    Update encryptConfigs.js

--- trainingportal/tools/encryptConfigs.js
+++ trainingportal/tools/encryptConfigs.js
@@ -3,29 +3,29 @@ const aescrypto = require(path.join(__dirname, '../aescrypto'));
 const util = require(path.join(__dirname, '../util'));
 
 //temporarily write your passwords here, cleanup after generating the encrypted settings
-var dbPass="<db pass>";//DELETE ME WHEN DONE
-var slackSecret="<slack secret>";//DELETE ME WHEN DONE
+//var dbPass="<db pass>";//DELETE ME WHEN DONE
+//var slackSecret="<slack secret>";//DELETE ME WHEN DONE
 var googleSecret="<google secret>";//DELETE ME WHEN DONE
-var badgrToken = "<badgr token>";
+//var badgrToken = "<badgr token>";
 
-var config = util.getConfig();
+//var config = util.getConfig();
 const fs = require('fs');
 
 console.log("======= config.json ==========");
 
 //You can use the following script from  when you update your key to a new value
-console.log(`"encDbPass": "${aescrypto.encrypt(dbPass)}",`);
-console.log(`"encSlackClientSecret": "${aescrypto.encrypt(slackSecret)}",`);
+//console.log(`"encDbPass": "${aescrypto.encrypt(dbPass)}",`);
+//console.log(`"encSlackClientSecret": "${aescrypto.encrypt(slackSecret)}",`);
 console.log(`"encGoogleClientSecret": "${aescrypto.encrypt(googleSecret)}",`);
-console.log(`"encBadgrToken": "${aescrypto.encrypt(badgrToken)}",`);
+//console.log(`"encBadgrToken": "${aescrypto.encrypt(badgrToken)}",`);
 
-
+/*
 if(!util.isNullOrUndefined(config.samlProviderPvkFilePath)){
     var samlProviderPvk = fs.readFileSync(path.join(__dirname, config.samlProviderPvkFilePath), 'utf-8');
     var encSamlProviderPvk = aescrypto.encrypt(samlProviderPvk);
     console.log(encSamlProviderPvk);
 } 
-
+*/
 
 /**
  * Use this function to generate the properties for a local user
